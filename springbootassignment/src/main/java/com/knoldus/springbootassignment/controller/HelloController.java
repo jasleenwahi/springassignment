b1ba6a74ea7fff6e56362f1011bc8de3ed11a707
jasleenwahi <124977275+jasleenwahi@users.noreply.github.com>
2023-04-09 17:53:32 +0530

    Update HelloController.java

--- springbootassignment/src/main/java/com/knoldus/springbootassignment/controller/HelloController.java
+++ springbootassignment/src/main/java/com/knoldus/springbootassignment/controller/HelloController.java
@@ -3,13 +3,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController  // this controller will recieve all the http requests.
 public class HelloController
 {
     @Autowired
-    PrintTime printTime;
-    @GetMapping("/status")
+    PrintTime printTime; // created a seperate class which contains method to print the time, autowiring the dependency here.
+    @GetMapping("/status") // here we are doing a get mapping when user hits this url the controller will recieve and will map it to this method and status will be didplayed.
     public String showStatus(){
-        return "Running " + printTime.showTime();
+        return "Running " + printTime.showTime(); 
     }
 }
