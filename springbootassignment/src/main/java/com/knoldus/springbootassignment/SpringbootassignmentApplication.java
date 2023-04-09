d7cbb7323ca9a954e1b53d178dbf9ca7ae4b764a
jasleenwahi <124977275+jasleenwahi@users.noreply.github.com>
2023-04-09 17:49:51 +0530

    Update SpringbootassignmentApplication.java

--- springbootassignment/src/main/java/com/knoldus/springbootassignment/SpringbootassignmentApplication.java
+++ springbootassignment/src/main/java/com/knoldus/springbootassignment/SpringbootassignmentApplication.java
@@ -3,10 +3,11 @@ package com.knoldus.springbootassignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // this annotation is combination of component scan, enable autoconfiguration, configuration.
 public class SpringbootassignmentApplication {
 
 	public static void main(String[] args) {
+		//this will automatically create the requored beans and boots up the application.
 		SpringApplication.run(SpringbootassignmentApplication.class, args);
 	}
 
